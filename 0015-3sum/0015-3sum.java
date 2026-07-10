e0e0a3841cacff148a3f6a0b72dd3a5a43fa589e
Ashis Kumar Singh <ashiskumarsingh755@gmail.com>
2026-07-10 21:26:52 +0530

    Time: 32 ms (79%), Space: 60.3 MB (12.14%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,3 +1,4 @@
+//Approch-1
 // class Solution {
 //        public List<List<Integer>> threeSum(int[] nums) {
 //         List<List<Integer>> result = new ArrayList<>();
@@ -21,44 +22,75 @@
 //     }
 // }
 
+//Approch-2
+// class Solution {
+//     public List<List<Integer>> threeSum(int[] nums) {
+//         int n = nums.length;
+//         // Stores unique triplets
+//         Set<List<Integer>> uniqueTriplets = new HashSet<>();
+//         for (int i = 0; i < n; i++) {
+//             HashSet<Integer> set = new HashSet<>();
+//             int target = -nums[i];
+//             for (int j = i + 1; j < n; j++) {
+//                 int third = target - nums[j];
+//                 if (set.contains(third)) {
+//                     List<Integer> triplet = new ArrayList<>();
+//                     triplet.add(nums[i]);
+//                     triplet.add(nums[j]);
+//                     triplet.add(third);
+//                     // Sort so duplicates become identical
+//                     Collections.sort(triplet);
 
+//                     uniqueTriplets.add(triplet);
+//                 }
+
+//                 set.add(nums[j]);
+//             }
+//         }
+
+//         return new ArrayList<>(uniqueTriplets);
+//     }
+// }
+
+//Approch-3
 class Solution {
-
     public List<List<Integer>> threeSum(int[] nums) {
-
         int n = nums.length;
-
-        // Stores unique triplets
-        Set<List<Integer>> uniqueTriplets = new HashSet<>();
-
+        Arrays.sort(nums);
+        int sum=0;
+        List<List<Integer>> result= new ArrayList<>();
         for (int i = 0; i < n; i++) {
-
-            HashSet<Integer> set = new HashSet<>();
-
-            int target = -nums[i];
-
-            for (int j = i + 1; j < n; j++) {
-
-                int third = target - nums[j];
-
-                if (set.contains(third)) {
-
-                    List<Integer> triplet = new ArrayList<>();
-
-                    triplet.add(nums[i]);
-                    triplet.add(nums[j]);
-                    triplet.add(third);
-
-                    // Sort so duplicates become identical
-                    Collections.sort(triplet);
-
-                    uniqueTriplets.add(triplet);
+            // if i will have equal to current value and previous value then
+            if(i>0 && nums[i]==nums[i-1])
+                continue;
+            int j=i+1,k=n-1;
+            while(j<k){
+                //find out the sum (Main oprations)
+                sum=nums[i]+nums[j]+nums[k];
+                //if negative
+                if(sum>0){
+                    k--;
+                //if postive
+                }else if(sum<0){
+                    j++;
+                }else{
+                    List<Integer> sublist= new ArrayList<>();
+                    sublist.add(nums[i]);
+                    sublist.add(nums[j]);
+                    sublist.add(nums[k]);
+                    result.add(sublist);
+                    j++;
+                    k--;
+                // if j will have equal to current value and previous value then
+                while (j<k && nums[j]==nums[j-1]){
+                    j++;
                 }
-
-                set.add(nums[j]);
+                while (j < k && nums[k] == nums[k + 1]){
+                    k--;
+                }
+                }
             }
         }
-
-        return new ArrayList<>(uniqueTriplets);
+        return result;
     }
 }
