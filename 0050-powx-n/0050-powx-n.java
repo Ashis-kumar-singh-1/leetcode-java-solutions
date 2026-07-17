9074b44555b6220398bc3d01519aa6bbc37f8f3b
Ashis Kumar Singh <ashiskumarsingh755@gmail.com>
2026-07-17 18:14:00 +0530

    Time: 0 ms (100%), Space: 48.3 MB (11.57%) - LeetHub

--- 0050-powx-n/0050-powx-n.java
+++ 0050-powx-n/0050-powx-n.java
@@ -3,3 +3,15 @@ class Solution {
         return Math.pow(x,n);
     }
 }
+
+// long MOD = 1000000007;
+//     public double myPow(double x, int n) {
+//         if(n==0){
+//             return 1;
+//         }
+//         double half=myPow(x,n/2);
+//         double ans = (half * half) % MOD;
+//         if(n % 2!=0)
+//             ans = (ans * x) % MOD;
+//             return ans;
+//     }
