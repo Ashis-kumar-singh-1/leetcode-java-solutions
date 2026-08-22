214204df10980ad288b0ec22c1a1f5a46526853c
Ashis Kumar Singh <ashiskumarsingh755@gmail.com>
2026-08-22 20:24:59 +0530

    Time: 12 ms (83.02%), Space: 43.5 MB (97.26%) - LeetHub

---
+++ 0863-all-nodes-distance-k-in-binary-tree/0863-all-nodes-distance-k-in-binary-tree.java
@@ -0,0 +1,71 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public List<Integer> distanceK(TreeNode root, TreeNode target, int k) {
+        List<Integer> ans = new ArrayList<>();
+        // Store parent of every node
+        Map<TreeNode, TreeNode> parent = new HashMap<>();
+        makeParent(root, null, parent);
+
+        // Queue for level order traversal
+        Queue<TreeNode> q = new LinkedList<>();
+        q.add(target);
+
+        // To avoid going back to the same node
+        Set<TreeNode> visited = new HashSet<>();
+        visited.add(target);
+        int level = 0;
+        while (!q.isEmpty()) {
+
+            // If we reached distance k
+            if (level == k) {
+                break;
+            }
+            int size = q.size();
+            // Process one complete level
+            for (int i = 0; i < size; i++) {
+                TreeNode curr = q.poll();
+                // Go left
+                if (curr.left != null && !visited.contains(curr.left)) {
+                    visited.add(curr.left);
+                    q.add(curr.left);
+                }
+
+                // Go right
+                if (curr.right != null && !visited.contains(curr.right)) {
+                    visited.add(curr.right);
+                    q.add(curr.right);
+                }
+
+                // Go to parent
+                TreeNode par = parent.get(curr);
+                if (par != null && !visited.contains(par)) {
+                    visited.add(par);
+                    q.add(par);
+                }
+            }
+            level++;
+        }
+
+        // Queue now contains nodes at distance k
+        while (!q.isEmpty()) {
+            ans.add(q.poll().val);
+        }
+        return ans;
+    }
+    public void makeParent(TreeNode root,TreeNode par,Map<TreeNode, TreeNode> parent) {
+        if (root == null) {
+            return;
+        }
+        parent.put(root, par);
+        makeParent(root.left, root, parent);
+        makeParent(root.right, root, parent);
+    }
+}
